ed549b2130f97b7fdab5ab513421af3d9c5ca581
Tomo Krajina <tkrajina@gmail.com>
2016-06-20 14:55:47 +0200

    Unwatchall

--- ftmpl.go
+++ ftmpl.go
@@ -12,7 +12,12 @@ import (
 )
 
 const (
+	watchLockFile = ".ftmpl_watch_"
+)
+
+const (
 	cmdlineWatch         = "watch"
+	cmdlineUnwatchAll    = "unwatchall"
 	cmdlineTargetGo      = "targetgo"
 	cmdlineTargetDir     = "targetdir"
 	cmdlineFuncPrefix    = "prefix"
@@ -21,13 +26,19 @@ const (
 
 func main() {
 	var params ftmplting.Params
-	var watch bool
+	var watch, unwatch bool
 	flag.StringVar(&(params.TargetGoFile), cmdlineTargetGo, "", "Merge the result in this go file")
 	flag.StringVar(&(params.TargetDir), cmdlineTargetDir, "", "Save the result in this directory")
 	flag.StringVar(&(params.FuncPrefix), cmdlineFuncPrefix, "TMPL", "Prefix to be used with generated functions")
 	flag.StringVar(&(params.FuncPrefixErr), cmdlineFuncPrefixErr, "TMPLERR", "Prefix to be used with generated functions (returning error)")
 	flag.BoolVar(&watch, cmdlineWatch, false, "Watch source directory and recompile templates if anything changes there")
+	flag.BoolVar(&unwatch, cmdlineUnwatchAll, false, "Stop all ftmpl watching processes started in this directory")
 	flag.Parse()
+
+	if unwatch {
+		unwatch()
+		os.Exit(0)
+	}
 
 	if len(flag.Args()) != 1 {
 		fmt.Fprintln(os.Stderr, "Need one source directory with templates")
@@ -51,10 +62,26 @@ func main() {
 	}
 }
 
+func unwatch() {
+	fileInfos, err := ioutil.ReadDir(".")
+	ftmplting.HandleError(err, "Error listing directory")
+
+	for _, fileName := range files {
+		if strings.HasPrefix(fileName, watchLockFile) {
+			err := os.Remove(fileName)
+			ftmplting.HandleError(err, "Cannot remove "+fileName)
+		}
+	}
+}
+
 func watchAndRecompile(params ftmplting.Params) {
 	fmt.Println("Watching for changes in", params.SourceDir)
 	watcher, err := fsnotify.NewWatcher()
 	ftmplting.HandleError(err, "Error starting watcher for "+params.SourceDir)
+
+	watchLockFile := watchLockFile + fmt.Sprintf("%d", os.Getpid())
+	_, err = os.Create(watchLockFile)
+	ftmplting.HandleError(err, "Cannot create "+watchLockFile)
 
 	done := make(chan bool)
 
@@ -62,17 +89,26 @@ func watchAndRecompile(params ftmplting.Params) {
 		for {
 			select {
 			case ev := <-watcher.Event:
-				if strings.HasSuffix(ev.Name, ".tmpl") {
-					fmt.Println(params.SourceDir, "changed => recompiling:", ev.Name)
-					ftmplting.Do(params)
+				if ev != nil {
+					if strings.HasSuffix(ev.Name, ".tmpl") {
+						fmt.Println(params.SourceDir, "changed => recompiling:", ev.Name)
+						ftmplting.Do(params)
+					}
+					if ev.Name == watchLockFile && ev.IsDelete() {
+						fmt.Println("Stop watching " + params.SourceDir)
+						os.Exit(0)
+					}
 				}
 			case err := <-watcher.Error:
-				panic(err.Error())
+				ftmplting.HandleError(err, "Error")
 			}
 		}
 	}()
 
 	err = watcher.Watch(params.SourceDir)
+	ftmplting.HandleError(err, "Error watching "+params.SourceDir)
+
+	err = watcher.Watch(watchLockFile)
 	ftmplting.HandleError(err, "Error watching "+params.SourceDir)
 
 	<-done
